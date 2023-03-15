891496dfe330975984698e4fb5b4853ccb74471c
TienLe <trantiendev@gmail.com>
2023-03-16 01:22:42 +0700

    setup app layout && next-auth

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,23 @@
+import { ChakraBaseProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
+import { theme } from '../chakra/theme';
 import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '@/lib/graphql/apollo-client';
+import { Toaster } from 'react-hot-toast';
 
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <ChakraBaseProvider theme={theme}>
+          <Component {...pageProps} />
+          <Toaster />
+        </ChakraBaseProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
